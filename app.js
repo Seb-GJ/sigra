991233f8b3f16340bd849aa8041b677f5e388d7b
Sebastian <sebastian23glz@gmail.com>
2020-11-15 21:44:14 -0600

    server worked

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require ('express'),
+/* var express = require ('express'),
     bodyParser = require ('body-parser'),
     cors = require ('cors');
 
@@ -23,4 +23,27 @@ var server = app.listen(process.env.PORT || 3000, function(){
 });
 
 
+ */
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
+// Objeto global de la app
+var app = express();
+
+// configuración de middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Manejando los errores 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Iniciando el servidor...
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('Escuchando en el puerto ' + server.address().port);
+});
